39982d8cba6c0a0e46db600ec617b92e0f0ae357
vardeath <30544416+vardeath@users.noreply.github.com>
2017-07-29 09:49:36 +0300

    Add files via upload

---
+++ ConsoleApplication7/ConsoleApplication7.cpp
@@ -0,0 +1,67 @@
+#define _SILENCE_STDEXT_HASH_DEPRECATION_WARNINGS
+#include "stdafx.h"
+#include<iostream>
+#include<fstream>
+#include<sstream>
+#include<cmath>
+#include<cstdlib>
+#include<string>
+#include<list>
+#include<vector>
+#include<algorithm>
+#include<stdexcept>
+#include <functional>
+#include<iomanip>
+#include "windows.h"
+//#include "../../std_lib_facilities.h"
+
+using namespace std;
+
+string oper_name = " "; // наименование операции
+						// выполняем рассчет выражения пользователя, возвращаем результат.
+double operation(double x, double y, char z) {
+	switch (z) {
+	case '+': oper_name = "Сумма"; return(x + y);
+	case '-': oper_name = "Разность"; return(x - y);
+	case '*': oper_name = "Произведение"; return(x * y);
+	case '/': oper_name = "Деление"; return(x / y);
+	}
+}
+
+int main()
+{
+	setlocale(LC_CTYPE, "RUS");
+	setlocale(LC_ALL, "");
+
+	char oper = ' '; // знак математической операции
+	string user_value1 = " "; // первое значение пользователя
+	string user_value2 = " "; // второе значение пользователя
+	double oper_trans1 = 0; // записываем значение первого операнда после перевода
+	double oper_trans2 = 0; // записываем значение первого операнда после перевода
+
+	vector<string> digits = { "zero", "one", "two", "three", "four", "five", "six", "seven","eight", "nine" };
+
+	cout << "введите 2 операнда и оператор" << endl;
+	cin >> user_value1 >> user_value2 >> oper;
+
+	for (int i = 0; i < digits.size(); ++i) {
+		if (user_value1 == digits[i]) {
+			oper_trans1 = i;
+		}
+		if (user_value2 == digits[i]) {
+			oper_trans2 = i;
+		}
+	}
+	if (oper_trans1 == 0) {
+		oper_trans1 = std::stod(user_value1);
+	}
+	if (oper_trans2 == 0) {
+		oper_trans2 = std::stod(user_value2);
+	}
+
+	double result = operation(oper_trans1, oper_trans2, oper);
+
+	cout << oper_name << " " << user_value1 << " и " << user_value2 << " " << "равно " << result << endl;
+
+	system("pause");
+}
